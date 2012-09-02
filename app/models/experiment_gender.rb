be3fc030d4d0b23fb9a40777dfcef9f1e05808d0
Siroberto Scerbo <github@siroberto.com>
2012-09-02 14:17:46 -0400

    Removed experiment genders

--- app/models/experiment_gender.rb
+++
@@ -1,5 +0,0 @@
-class ExperimentGender < ActiveRecord::Base
-  belongs_to :gender
-  belongs_to :experiment
-  # attr_accessible :title, :body
-end
--- app/models/gender.rb
+++ app/models/gender.rb
@@ -1,6 +1,3 @@
-class Gender < ActiveRecord::Base
-  has_many :experiment_genders
-  has_many :experiments, :through => :experiment_genders
-  
+class Gender < ActiveRecord::Base  
   attr_accessible :gender
 end
