5fd5e604d99e2f4ad0966444c1a1d229fbc8c973
Siroberto Scerbo <github@siroberto.com>
2012-09-02 14:14:02 -0400

    fixed category_tokens definition

--- app/models/task.rb
+++ app/models/task.rb
@@ -9,11 +9,11 @@ class Task < ActiveRecord::Base
 
   attr_reader :category_tokens
 
-  def display_tokens=(ids)
+  def category_tokens=(ids)
     ids.gsub!(/CREATE_(.+?)_END/) do
       Category.create!(:task_category => $1).id
     end
-    self.display_ids = ids.split(",")
+    self.category_ids = ids.split(",")
   end
 
   attr_accessible :env_desc, :interface_desc, :task_desc, :title, :category_tokens, :dimension_id
