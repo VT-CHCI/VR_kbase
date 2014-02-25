ce109642b0d38d9c21d39ac3731f9d1bdb0f3e70
Siroberto Scerbo <github@siroberto.com>
2014-02-25 11:09:57 -0500

    fixed mislabeled input causing category_ids assign problem

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,5 +2,7 @@ class Category < ActiveRecord::Base
   has_many :task_categories
   has_many :tasks, :through => :task_category
 
+  has_one :finding
+
   attr_accessible :task_category
 end
--- app/models/metric.rb
+++ app/models/metric.rb
@@ -1,3 +1,7 @@
 class Metric < ActiveRecord::Base
+	has_many :task_metrics
+  has_many :tasks, :through => :task_metric
+
+  has_one :finding
   attr_accessible :metric
 end
