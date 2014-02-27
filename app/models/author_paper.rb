da2d95fa7b35fc3e8a1a61c766ae3b693c41ccb9
Felipe Bacim <fbacim@vt.edu>
2014-02-27 14:59:27 -0500

    We now check if author is already in the database when creating a paper. If it exists, use existing author instead of creating a new entry in the database.

--- app/models/author_paper.rb
+++ app/models/author_paper.rb
@@ -1,8 +1,19 @@
 class AuthorPaper < ActiveRecord::Base
-  belongs_to :author
+  belongs_to :author, :autosave => true
   belongs_to :paper
   
   accepts_nested_attributes_for :author
 
   attr_accessible :order, :author_attributes
+
+  # If you need to validate the associated record, you can add a method like this:
+  #     validate_associated_record_for_author
+  def autosave_associated_records_for_author
+    # Find or create the author by name
+    if existing_author = Author.where(:last_name => author.last_name, :first_name => author.first_name, :middle_initial => author.middle_initial).first
+      self.author = existing_author
+    else
+      self.author = author
+    end
+  end
 end
