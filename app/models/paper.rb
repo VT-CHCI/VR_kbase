8c60d067b5806e562be49babe8f36c9f95a6c039
Siroberto Scerbo <github@siroberto.com>
2014-02-27 22:03:02 -0500

    paper view almost complete

--- app/models/paper.rb
+++ app/models/paper.rb
@@ -5,12 +5,12 @@ class Paper < ActiveRecord::Base
   has_many :paper_venues, :dependent => :destroy
   has_many :venues, :through => :paper_venues
 
-  # accepts_nested_attributes_for :authors
   accepts_nested_attributes_for :author_papers
   accepts_nested_attributes_for :experiments
   accepts_nested_attributes_for :venues
 
   attr_accessible :doi, :end_page, :num_views, :paper_url, :start_page, :title, :volume,
-    :year, :issue, :venues_attributes, :experiments_attributes, #:authors_attributes, 
-    :author_papers_attributes, :tasks_attributes, :findings_attributes, :category_ids
+    :year, :issue, :venues_attributes, :experiments_attributes, :author_papers_attributes, 
+    :tasks_attributes, :findings_attributes, :category_ids
+
 end
